164c403dd6103eb3cd0a902376519ac3a4bdc2b7
Alexander Nachtigal <Alexander.Nachtigal@gmail.com>
2022-06-16 15:19:16 +0200

    temp

--- src/game_elements/GameCharacter.java
+++ src/game_elements/GameCharacter.java
@@ -4,7 +4,7 @@
  * @version 1.0
  * @authors Veronica Zylla, Sören Zacharias, Alexander Nachtigal
  * @email veronica.zylla@student.fh-kiel.de, soeren.zacharias@student.fh-kiel.de, alexander.nachtigal@student.fh-kiel.de
- * @description abstract class for all characters in this game.
+ * @description abstract class for all characters(survivors & zombies) in this game.
  */
 
 package game_elements;
